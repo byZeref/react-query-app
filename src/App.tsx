4718c769a2a175e3efeced9a83a7ccb90f8441c1
sandorsc <sandorsc941018@gmail.com>
2025-11-22 10:24:49 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <main className='w-full flex flex-col items-center p-20'>
+      <h1 className='text-4xl font-semibold'>React Query App</h1>
+    </main>
+  )
+}
+
+export default App
